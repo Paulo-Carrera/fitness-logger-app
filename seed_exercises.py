526a356d2a3fcea470bf4eff23990990ea97cf8f
Paulo <paulo3252@myyahoo.com>
2025-05-23 16:39:21 -0700

    commit

--- seed_exercises.py
+++ seed_exercises.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-import urllib.parse
 import json
 
 from models import Exercise, db
@@ -8,18 +7,6 @@ from app import app
 
 # Load environment variables
 load_dotenv()
-
-# Read from .env
-db_user = os.getenv('DB_USER')
-db_password = urllib.parse.quote_plus(os.getenv('DB_PASSWORD'))  # URL-encode special characters
-db_host = os.getenv('DB_HOST')
-db_name = os.getenv('DB_NAME')
-
-# Build connection string
-manual_db_url = f'postgresql://{db_user}:{db_password}@{db_host}/{db_name}'
-os.environ['DATABASE_URL'] = manual_db_url
-
-# print("Using DATABASE_URL:", os.getenv('DATABASE_URL'))
 
 def seed_exercises():
     json_path = os.path.join(os.path.dirname(__file__), 'static/data/exercises.json')
@@ -29,7 +16,11 @@ def seed_exercises():
                 exercises = json.load(f)
 
             for exercise in exercises:
-                ex = Exercise(id=exercise['id'], name=exercise['name'], description=exercise.get('description'))
+                ex = Exercise(
+                    id=exercise['id'],
+                    name=exercise['name'],
+                    description=exercise.get('description')
+                )
                 db.session.merge(ex)
 
             db.session.commit()
@@ -39,4 +30,3 @@ def seed_exercises():
 
 if __name__ == '__main__':
     seed_exercises()
-
